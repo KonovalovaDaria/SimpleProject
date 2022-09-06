95bef3c40be9c3835311fd47a48a27d682f89970
Konovalova Daria <konovalova@skydns.ru>
2022-09-06 18:38:56 +0500

    update services

--- core/services.py
+++ core/services.py
@@ -3,65 +3,128 @@
 Один работает напрямую с валидатором, данными и репо.
 Второй работает через форму.
 """
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+
 from django.core.exceptions import ValidationError
 
+from core.logic import AbstractLogic
+from core.repository import AbstractRepository
+from core.serializers import AbstractSerializer
+from core.validators import AbstractValidator
 
-class Service:
+
+class IServiceExecutable(ABC):
     _success = None
     _errors = None
 
-    def __init__(self, validator=None, repo=None, serializer=None):
-        self._repo = repo
+    @abstractmethod
+    def execute(self, context: Any, **kwargs) -> Any:
+        pass
+
+    @property
+    @abstractmethod
+    def success(self) -> Optional[bool]:
+        return self._success
+
+    @property
+    @abstractmethod
+    def errors(self) -> Any:
+        return self._errors
+
+
+class AbstractService(IServiceExecutable, ABC):
+    """Абстрактный сервис."""
+
+    def __init__(
+        self,
+        validator: Optional[AbstractValidator] = None,
+        repo: Optional[AbstractRepository] = None,
+        form: Optional[AbstractForm] = None,
+        logic: Optional[AbstractLogic] = None,
+        serializer: Optional[AbstractSerializer] = None,
+        call_back: Optional[IServiceExecutable] = None,
+        on_error: Optional[IServiceExecutable] = None,
+    ):
         self._validator = validator
+        self._repo = repo
+        self._form = form
+        self._logic = logic
         self._serializer = serializer
+        self._call_back = call_back
+        self._on_error = on_error
+
+        if not self._repo or not self._logic:
+            raise TypeError('repo or logic required')
+
+
+class RepoService(AbstractService, ABC):
+
+    __slots__ = ('_validator', '_repo', '_serializer', '_call_back', '_on_error')
+
+    def __init__(self, *args, **kwargs):
+        super(RepoService, self).__init__(*args, **kwargs)
+
+        if not self._repo:
+            raise TypeError('repo is required')
 
     def execute(self, context, **kwargs):
         try:
             if self._validator:
-                self._validator(**context)
+                self._validator.execute(**context)
         except ValidationError as ex:
             self._success = False
             self._errors = ex.messages
         else:
             self._success = True
-            if self._repo:
-                result = self._repo(**context)
-                if self._serializer:
-                    return self._serializer(result)
-                return result
-
-    @property
-    def success(self):
-        return self._success
-
-    @property
-    def errors(self):
-        return self._errors
+            result = self._repo.execute(**context)
+            if self._serializer:
+                return self._serializer.execute(result)
+            return result
 
 
-class FormService:
-    _success = None
-    _errors = None
+class LogicService(AbstractService, ABC):
 
-    def __init__(self, form, serializer=None):
-        self._form = form
-        self._serializer = serializer
+    __slots__ = ('_validator', '_logic', '_serializer', '_call_back', '_on_error')
+
+    def __init__(self, *args, **kwargs):
+        super(LogicService, self).__init__(*args, **kwargs)
+
+        if not self._logic:
+            raise TypeError('logic is required')
+
+    def execute(self, context, **kwargs):
+        try:
+            if self._validator:
+                self._validator.execute(**context)
+        except ValidationError as ex:
+            self._success = False
+            self._errors = ex.messages
+        else:
+            self._success = True
+            result = self._logic.execute(**context)
+            if self._serializer:
+                return self._serializer.execute(result)
+            return result
+
+
+class FormService(AbstractService, ABC):
+
+    __slots__ = ('_validator', '_form', '_serializer', '_call_back', '_on_error')
+
+    def __init__(self, *args, **kwargs):
+        super(FormService, self).__init__(*args, **kwargs)
+
+        if not self._form:
+            raise TypeError('form required')
 
     def execute(self, **kwargs):
         if self._form.is_valid():
             self._success = True
             result = self._form.save()
             if self._serializer:
-                return self._serializer(result)
+                return self._serializer.execute(result)
             return result
         else:
             self._success = False
             self._errors = self._form.errors
-
-    @property
-    def success(self):
-        return self._success
-
-    @property
-    def errors(self):
-        return self._errors
--- core/validators.py
+++ core/validators.py
@@ -1,25 +1,41 @@
 """
 Универсальных валидаторы, принемают репо с помощью которой производится проверка.
 """
+from abc import ABC, abstractmethod
+from typing import Any
+
 from django.core.exceptions import ValidationError
 
 
-class ObjectExistValidator:
+class AbstractValidator(ABC):
+
+    @abstractmethod
+    def execute(self, *args, **kwargs) -> Any:
+        pass
+
+
+class ObjectExistValidator(AbstractValidator):
+
+    __slots__ = ('_check_exist_by_id', '_error_message')
+
     def __init__(self, check_exist_by_id, error_message):
         self._check_exist_by_id = check_exist_by_id
         self._error_message = error_message
 
-    def __call__(self, author_id):
+    def execute(self, author_id):
         if not self._check_exist_by_id(author_id):
             raise ValidationError(self._error_message)
 
 
-class ObjectNotExistValidator:
+class ObjectNotExistValidator(AbstractValidator):
+
+    __slots__ = ('_check_exist', '_error_message', '_obj_id')
+
     def __init__(self, check_exist, error_message, obj_id=None):
         self._check_exist = check_exist
         self._error_message = error_message
         self._obj_id = obj_id
 
-    def __call__(self, **context):
+    def execute(self, **context):
         if self._check_exist(**context, obj_id=self._obj_id):
             raise ValidationError(self._error_message)
